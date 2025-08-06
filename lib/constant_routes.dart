309da5b505ebd0bae8a2a3177a5ccebc9da3f94a
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-06 11:48:48 +0530

    constant_routes.dart : all constant routes add

--- lib/constant_routes.dart
+++ lib/constant_routes.dart
@@ -1,3 +1,4 @@
-const String kHomeScreen = '/';
+const String kSplashScreen = '/';
+const String kHomeScreen = '/home_screen';
 const String kCategoryScreen = '/category_screen';
 const String kProductDetailsScreen = '/product_details_screen';
