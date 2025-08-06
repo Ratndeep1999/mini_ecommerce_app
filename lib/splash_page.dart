e12de7cf2b2087c8f38ab01f5ec2ee1e97c7f03d
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-06 12:10:53 +0530

    splash_page.dart : ui add, navigation add

--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mini_ecommerce_app/constant_routes.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -8,17 +9,39 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3), () => _navigateToHomeScreen());
+    super.initState();
+  }
+
+  // method to navigate home screen
+  void _navigateToHomeScreen(){
+    Navigator.pushReplacementNamed(context, kHomeScreen);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Text(
-            'e-commerce',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Colors.orange,
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.9,
+            width: MediaQuery.of(context).size.width * 0.9,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(40),
+              color: Colors.orangeAccent.shade100,
+            ),
+            child: Center(
+              child: Text(
+                'e-commerce',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+              ),
             ),
           ),
         ),
